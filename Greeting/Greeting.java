77a675b94b5f622577019d023f6098d49ee60239
Rico Tiongson <thericotiongson@gmail.com>
2015-01-04 20:09:52 +0800

    Reupdated with 'unpackaged' branch.

--- Greeting/Greeting.java
+++ Greeting/Greeting.java
@@ -9,17 +9,34 @@
  *
  * @author Rico Tiongson
  */
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 
 public class Greeting extends JFrame {
     public static Greeting Main = null;
     public int FPS;
     public ShapeCanvas canvas;
+    List<Timer> timers;
+    
+    public JToggleButton animateButton;
+    
     public static void main(String[] args) {
-        new Greeting(600, 400, 24).setVisible(true);
+        new Greeting(600, 425, 24).setVisible(true);
     }
+    
+    public void addTimer(Timer t) {
+        if (timers == null) timers = new ArrayList<>();
+        timers.add(t);
+        if (animateButton.isSelected())
+            t.start();
+        else
+            t.stop();
+    }
+    
     public Greeting(int width, int height, int FPS) {
         super();
         this.FPS = FPS;
@@ -28,15 +45,41 @@ public class Greeting extends JFrame {
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        canvas = new Layers();
+        timers = new ArrayList<>();
+        animateButton = new JToggleButton("Animate");
+        animateButton.addActionListener(
+            new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JToggleButton button = (JToggleButton) e.getSource();
+                    if (timers == null) return;
+                    if (button.isSelected()) {
+                        for (int i = 0, I = timers.size(); i < I; ++i)
+                            timers.get(i).start();
+                        button.setText("Pause");
+                    }
+                    else {
+                        for (int i = 0, I = timers.size(); i < I; ++i)
+                            timers.get(i).stop();
+                        button.setText("Animate");
+                    }
+                }
+                    
+            }
+        );
         
-        new Timer(1000 / FPS, new ActionListener() {
+        this.addTimer(new Timer(1000 / FPS, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 canvas.animateAll();
             }
-        }).start();
+        }));
         
+        
+        
+        canvas = new Layers();
         this.add(canvas);
+        this.add(animateButton, BorderLayout.SOUTH);
     }
 }
